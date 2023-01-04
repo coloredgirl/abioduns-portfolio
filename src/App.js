32ea5533adac2d9d35efea0bc5af094860c66973
Peace Abiodun <abiodunpeace8@gmail.com>
2023-01-04 17:20:52 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import NavBar from "./NavBar";
-import Header from "./Header";
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import About from "./About";
-import Contact from "./contact";
-import Skills from "./skills";
+import MainPage from "./mainPage";
 
 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-         <NavBar /> 
-        <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <>
-              <Header />
-               <About /> 
-               <Skills />
-               <Contact />
-              </>
-            </Route>
-
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <MainPage />
+    </div>
   );
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,5 @@
 import { useRef, useState } from "react";
 import { FaBars } from "react-icons/fa";
-import NavItems from "./Nav-Items";
 
 
 
@@ -10,15 +9,10 @@ const NavBar = () => {
 
     return (
         <nav className="navbar">
-            <h1>Portfolio</h1>
+            <h1>Frontend Engineer</h1>
             <div className={`menu ${state?'dropDown' :""}`}> 
                 <a href="#about">about</a>
-                <a href="https://github.com/peaceabiodun" 
-                style={{ 
-                color: 'white', 
-                backgroundColor: '#f1356d',
-                borderRadius: '8px'
-                 }} >work</a>
+                <a href="https://github.com/peaceabiodun" >work</a>
                 <a href= "https://opensea.io/collection/rangeofangels">NFT project</a>
                 <a href="#contact"> contact</a>
             </div>
