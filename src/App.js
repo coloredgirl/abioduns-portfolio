f947f777e9d17466c620fc7296e96b19cd710a7e
Peace Abiodun <abiodunpeace8@gmail.com>
2023-01-05 12:01:54 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MainPage from "./components/mainPage";
 
 function App() {
   return (
-    <div className="App">
+    <div className=' block py-0 px-[10%] md:px-[20%] h-[100vh] top-0 bottom-0 left-0 right-0 fixed'>
       <MainPage />
     </div>
   );
--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -4,21 +4,19 @@ FaReact, FaGit, FaNode} from "react-icons/fa"
 import { GrGraphQl } from "react-icons/gr";
 import { SiRedux } from "react-icons/si";
 import { MdEmail } from "react-icons/md";
+import { SiTailwindcss} from "react-icons/si";
 
 const MainPage = () => {
     return (
-        <div className='mx-7 my-5 flex flex-col h-[100%]'>
+        <div className='flex h-[80%] justify-center items-center '>
+            <div className=''>
            
-            <div>
-                <h1 className='text-sm md:text-lg font-bold'>Frontend Engineer</h1>
-            </div>
+            <main  className='flex flex-col justify-center items-start '>
+                <h1 className='text-sm md:text-lg font-semibold'>Frontend Engineer</h1> 
+                <h1 className='text-2xl md:text-4xl font-bold mb-1'>Hi 👋, I'm Peace </h1>
+                <h2 className='text-sm md:text-lg m-0'>I build responsive and well optimized software products.</h2>
 
-            <main>
-                <div className='flex flex-col text-center items-center justify-center mt-[220px]'>
-                    <h1 className='text-2xl md:text-4xl font-bold'>Hi 👋, I'm Peace </h1>
-                    <h2 className='text-sm md:text-lg'>I build responsive and well optimized software products.</h2>
-                </div>
-                <div className='flex flex-row items-center justify-center mt-3 gap-3 mb-[100px]' >
+                <div className='flex flex-wrap items-center justify-center mt-3 gap-3' >
                     <a href="https://github.com/peaceabiodun"><FaGithub size={25} /></a>
                     <a href="https://www.linkedin.com/in/peaceabiodun"><FaLinkedin size={25} /></a>
                     <a href="https://twitter.com/_fine_peace_"><FaTwitter size={25} /></a>
@@ -27,24 +25,52 @@ const MainPage = () => {
             </main>  
 
 
-            <footer className=' fixed left-0 bottom-2 w-[100%] text-center flex flex-col py-2 justify-center items-center'>
-            <span className='flex flex-row gap-3'>
-                    <FaHtml5 size={30} />
-                    <FaCss3 size={30} />
-                    <FaReact size={30} />
-                    <FaJsSquare size={30} />
+            <div className='flex flex-col justify-center items-center h-[20vh] w-[100%] fixed left-0 bottom-2 '>
+
+                <span className='flex flex-row gap-3'>
+                    <div>
+                        <FaHtml5 id='icon' size={30} className='cursor-pointer' />
+                        <p id='show' className='hidden top-[-15px] text-xs font-semibold text-center absolute z-10 cursor-pointer bg-white border-2 rounded-lg p-2'>html5</p>
+                    </div>
+                    <div>
+                        <SiTailwindcss id='icon' size={30} className='cursor-pointer'  />
+                        <p id='show' className='hidden top-[-15px] text-xs font-semibold text-center absolute z-10 cursor-pointer bg-white border-2 rounded-lg p-2'>Tailwindcss</p>
+                    </div>
+                    <div>
+                        <FaReact id='icon' size={30} className='cursor-pointer'  />
+                        <p id='show' className='hidden top-[-15px] text-xs font-semibold text-center absolute z-10 cursor-pointer bg-white border-2 rounded-lg p-2'>React.js</p>
+                    </div>
+                    <div>
+                        <FaJsSquare id='icon' size={30} className='cursor-pointer'  />
+                        <p id='show' className='hidden top-[-15px] text-xs font-semibold text-center absolute z-10 cursor-pointer bg-white border-2 rounded-lg p-2'>JavaScript</p>
+                    </div>
+
                 </span>
+
                 <span className='flex flex-row gap-3 my-2 '>
-                    <FaNode size={30} />
-                    <FaGit size={30} />
-                    <GrGraphQl size={30} />
-                    <SiRedux size={30} />
+                    <div>
+                        <FaNode  id='icon' size={30} className='cursor-pointer'  />
+                        <p id='show' className='hidden top-6 text-xs font-semibold text-center absolute z-10 cursor-pointer bg-white border-2 rounded-lg p-2'>Node</p>
+                    </div>
+                    <div>
+                        <FaGit  id='icon' size={30} className='cursor-pointer'  />
+                        <p id='show' className='hidden top-6 text-xs font-semibold text-center absolute z-10 cursor-pointer bg-white border-2 rounded-lg p-2'>Git</p>
+                    </div>
+                    <div>
+                        <GrGraphQl  id='icon' size={30} className='cursor-pointer'  />
+                        <p id='show' className='hidden top-6 text-xs font-semibold text-center absolute z-10 cursor-pointer bg-white border-2 rounded-lg p-2'>GraphQL</p>
+                    </div>
+                    <div>
+                        <SiRedux  id='icon' size={30} className='cursor-pointer'  />
+                        <p id='show' className='hidden top-6 text-xs font-semibold text-center absolute z-10 cursor-pointer bg-white border-2 rounded-lg p-2'>Redux</p>
+                    </div>
                 </span>
+
                 <span className='text-sm flex justify-center items-center'>© 2023 peace abiodun </span>
-            </footer>
+            </div>
 
 
-
+            </div>
 
         </div>
     );
